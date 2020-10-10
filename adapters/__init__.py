e925e2054b4be808b5c60f2db67d041d40d09839
bbbki64 <benoit@berges-ragoche.fr>
2020-10-10 19:13:06 +0200

    Validation sur la branche Master

--- adapters/__init__.py
+++ adapters/__init__.py
@@ -137,6 +137,7 @@ adapter_by_model = dict({
     'N2G-SP': OnOffKwhAdapter,          # NET2GRID N2G-SP
     # Nue
     'FB56+ZSW05HG1.2': OnOffSwitchAdapter,      # Nue ZigBee one gang smart switch
+    'LXN59-1S7LX1.0': OnOffSwitchAdapter,      # Nue ZigBee one gang smart switch
     'HGZB-01A': OnOffSwitchAdapter,     # Nue ZigBee smart light controller
     'HGZB-02A': DimmableBulbAdapter,    # Nue / 3A ZigBee smart light controller
     # Oujiabao
